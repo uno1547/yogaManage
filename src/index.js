48a6383ee98f773668eff57d3f48b0bcf3dd35b4
uno1547 <63580165+uno1547@users.noreply.github.com>
2024-02-26 04:05:57 +0900

    fullCalendar에 이벤트 추가
    원하는 날짜에 이벤트 표시 가능, firestore에서 불러온 결제내역을 종합해 일일매출을 이벤트로써 표시해야함

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
-import { getFirestore, collection, addDoc, getDocs, doc , getDoc } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+import { getFirestore, collection, getDocs, query, where } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+//firebase app객체 만들고 실행!
 const app = initializeApp({
   apiKey: "AIzaSyBykm-oqoMvIAjLFWHPnVi_OQ86Iis_NVs",
   authDomain: "yoga-663cb.firebaseapp.com",
@@ -8,78 +9,15 @@ const app = initializeApp({
   messagingSenderId: "256248240983",
   appId: "1:256248240983:web:07dcebbcb04debc34b3c12"
 })
-const db = getFirestore(app)
-
-// function getFsMember () {
-  const docRef = doc(db, 'members', "tvn2fZd8eEUKn8l4d4Co")
-  const docMem = await getDoc(docRef)
-  // return docMem.data()
-// }
-//시작하기의 웹 모듈식 API 새 컬렉션에 첫 문서 저장 성공 !!
-/*
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-*/
-//위의 users컬렉션에 새 문서 저장 성공
-/*
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Alan",
-    middle: "Mathison",
-    last: "Turing",
-    born: 1912
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-*/
-//위의 users컬럭션의 문서들 조회성공!
-/*
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
-*/
-// members의 문서 읽기성공 완전하지는않은듯
-// const querySnapshot = await getDocs(collection(db, "members"));
-// querySnapshot.forEach((doc) => {
-//   console.log(`${doc.id} => ${doc.data()}`);
-// });
-// const docRef = doc(db, "members", "0XjIgUezbBYDu4HUBWjw");
-// const docSnap = await getDoc(docRef);
-// if (docSnap.exists()) {
-//   console.log("Document data:", docSnap.data());
-// } else {
-//   // docSnap.data() will be undefined in this case
-//   console.log("No such document!");
-// }
-//시작하기의 데이터 조회(web module API)
-// const querySnapshot = getDocs(collection(db, "fbMembers"))
-// querySnapshot.forEach((doc) => console.log(doc.id, doc.data()) )
-
-/*
-const docRef = doc(db, 'members', "tvn2fZd8eEUKn8l4d4Co")
-const docSnap = getDoc(docRef)
-console.log(docSnap.data())
-*/
-
-
+//페이지 로드시 현재 날짜를 따서, 이번달 달력에 현재 날짜까지 표시
 document.addEventListener('DOMContentLoaded', function () {
-  getFsMember()
+  let currentDate = getDate() //현재 날짜 [02, 26]
+  let eventsArr = getEvents(...currentDate) //event객체배열 
+  console.log(eventsArr)
+
   const calendarEl = document.querySelector('.calender')
-  const a = 19
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView : 'dayGridMonth',
-    // selectable : true,
     buttonText : {
       today : '오늘'
     },
@@ -87,18 +25,75 @@ document.addEventListener('DOMContentLoaded', function () {
       start : '',
       center : 'title'
     },
-    dateClick : function (info) {
-      console.log(info)
-    },
-    events : [{
-      id : 'a',
-      title : `${a}`,
-      start : '2024-02-10',
-      end : '2024-02-10'
-    }]
+    events : eventsArr
   })
   calendar.render()
-  const event = calendar.getEventById('a')
-  const start = event.start
-  // console.log(start)
 })
+
+//현재 날짜에대한 [월, 일]을 반환
+function getDate() {
+  let date = new Date()
+  let month = date.getMonth() + 1
+  month = String(month).padStart(2, '0')
+  let day = date.getDate()
+  return [month, day]
+}
+//날짜별로 이벤트객체배열 반환
+function getEvents(month, day) {
+  console.log(month, day)
+  let events = []
+  //1일부터 day일 까지의 event객체 배열을 생성후 반환
+  for(let i = 1; i <= day; i++) {
+    let evt = {
+      title : `누적매출 : ${i}`,
+      start : `2024-${month}-${String(i).padStart(2, '0')}`,
+      end : `2024-${month}-${String(i).padStart(2, '0')}`
+    }
+    events.push(evt)
+  }
+  return events
+}
+const db = getFirestore(app)
+const q = query(collection(db, "payments"), where("pay_day", "<=", 11)); //현재날짜 이전의 모든 결제내역이 담긴 배열
+const querySnapshot = await getDocs(q);
+// console.log(querySnapshot)
+getDaySales(querySnapshot)
+function getDaySales(queries) {
+  let arrTofunc = []
+  queries.forEach((doc) => { 
+    let pay = {
+      pay_month : doc.data().pay_month,
+      pay_day : doc.data().pay_day,
+      pay_fee : doc.data().fee,
+    }
+    arrTofunc.push(pay)
+    // doc.data() is never undefined for query doc snapshots
+    // console.log(doc.id, " => ", doc.data());
+    // console.log(doc.data().fee)
+  })
+  console.log(arrTofunc) //정상 객체배열형태로 변환
+  for(a of arrTofunc) {
+    console.log(a)
+    let sale = {}
+    if (sale.pay_day) {
+      console.log(sale)
+    }
+  }
+}
+
+/*
+async function getFsMember() {
+  const db = getFirestore(app)
+  const q = query(collection(db, "payments"), where("pay_date", ">=", 20));
+  let sum = 0
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+    console.log(doc.data().fee)
+    sum += doc.data().fee
+  });
+  return sum
+}
+*/
+//결제내역 배열을 받아 날짜별로 일매출을 저장후 반환
