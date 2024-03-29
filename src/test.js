825ea210cf33d119975ca3f21e85ae928de18bc8
uno1547 <63580165+uno1547@users.noreply.github.com>
2024-03-29 21:34:25 +0900

    test - scrollTo problem fixed
    change #drop-down 'overflow-y : hidden' to 'overflow-y : auto/scroll'
    scrollTo ({option}) work!

--- src/test.js
+++ src/test.js
@@ -1,5 +1,29 @@
-const a = "010-9928-1547"
-console.log(a.length);
-for (let ch of a) {
-  console.log(ch, a.indexOf(ch));
-}
+/*
+const scrollEl = document.querySelector('div')
+scrollEl.addEventListener("mouseleave", function() {
+  scrollEl.scrollTo({
+    top : 0,
+    behavior : 'smooth'
+  })
+})
+
+*/
+/*
+const scrollTable1 = document.querySelector('table#first-table')
+scrollTable1.addEventListener("mouseleave", function() {
+  scrollTable1.scrollTo({
+    top : 0,
+    behavior : 'smooth'
+  })
+})
+*/
+/*
+*/
+const scrollTable2 = document.querySelector('table#second-table')
+scrollTable2.addEventListener("mouseleave", function() {
+  console.log('out');
+  scrollTable2.scrollTo({
+    top : 0,
+    behavior : 'smooth'
+  })
+})
