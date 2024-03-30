1df0db690881d24e40fe0404a20d7d73a5d06615
uno1547 <63580165+uno1547@users.noreply.github.com>
2024-03-30 12:58:29 +0900

    set paymentData obj

--- src/new-payment.js
+++ src/new-payment.js
@@ -1,9 +1,37 @@
 const searchParams = new URLSearchParams(window.location.search)
 const receivedId = Number(searchParams.get('user_id'))
 console.log(receivedId)
-const classInfo = {
-  pt : {},
-  gp : {}
+class Class {
+  constructor(type, perWeek, term, cashPrice, creditPrice) {
+    this.class_type = type,  
+    this.times_a_week = perWeek,  
+    this.class_term = term,  
+    this.cash_price = cashPrice,
+    this.credit_price = creditPrice  
+  }
+}
+const classDic = {
+  pt : new Class('pt', 10, 3, 600000, 600000),
+  group_try : new Class('group', 30, 3, 0, 0),
+  group_coupon : new Class('group', 10, 3, 230000, 250000),
+  group_two_one : new Class('group', 2, 1, 129000, 143000),
+  group_two_three1 : new Class('group', 2, 3, 330000, 367000),
+  group_two_three2 : new Class('group', 2, 3, 347000, 386000),
+  group_three_one : new Class('group', 3, 1, 149000, 165000),
+  group_three_three1 : new Class('group', 3, 3, 360000, 400000),
+  group_three_three2 : new Class('group', 3, 3, 378000, 420000),
+  group_three_six1 : new Class('group', 3, 6, 677000, 752000),
+  group_three_six2 : new Class('group', 3, 6, 713000, 792000),
+  group_three_twelve1 : new Class('group', 3, 12, 1190000, 1320000),
+  group_three_twelve2 : new Class('group', 3, 12, 1250000, 1390000),
+  group_four_twelve : new Class('group', 4, 12, 1150000, 0),
+  group_five_one : new Class('group', 5, 1, 168000, 187000),
+  group_five_three1 : new Class('group', 5, 3, 408000, 453000),
+  group_five_three2 : new Class('group', 5, 3, 429000, 477000),
+  group_five_six1 : new Class('group', 5, 6, 768000, 853000),
+  group_five_six2 : new Class('group', 5, 6, 810000, 898000),
+  group_five_twelve1 : new Class('group', 5, 12, 1340000, 1490000),
+  group_five_twelve2 : new Class('group', 5, 12, 1410000, 1570000),
 }
 // table 및 하위요소에서 click 발생시 해당줄의 radio활성화
 const table = document.querySelector("table#drop-down")
@@ -30,7 +58,8 @@ table.addEventListener("mouseleave", function() {
   if (isRadioChecked) { //체크된요소가존재
     const checkedRow = isRadioChecked.closest('tr')
     const checkedRowIndex = Array.from(table.querySelectorAll('tr')).indexOf(checkedRow)
-    // const rowHeight = checkedRow.offsetHeight 이걸로하면 56.53 이 올려져서 57이할당됨
+    // const rowHeight = checkedRow.offsetHeight 
+    //이걸로하면 56.53 이 올려져서 57이할당됨
     // 이렇게 되면 조금씩 오차가 커져서 나중에 스크롤된 위치가 안맞게됨
     const rowHeight = 56.57000006
     // const rowHeight = 56.77000006
@@ -51,10 +80,10 @@ table.addEventListener("mouseleave", function() {
 const form = document.querySelector('form')
 form.addEventListener("submit", function(evt) {
   evt.preventDefault()
-  console.log('submit!!');
+  // console.log('submit!!');
   const formData = new FormData(evt.target)
   const memberObj = Object.fromEntries(formData)
-  console.log('form으로얻은 formdata :',memberObj)
+  // console.log('form으로얻은 formdata :',memberObj)
   // // { class : 'group', method : 'credit' , teacher : '김영원' }
   setPaymentObj(memberObj) 
 })
@@ -69,13 +98,23 @@ function setPaymentObj(obj) {
   payment.pay_teacher = obj.teacher
   payment.pay_method = obj.method
   const payKey = obj.class
-  console.log(payment);
+  // console.log(payment);
+  // console.log(payKey);
   setPaymentClassInfo(payment, payKey)
 }
 // 넘어온 obj.class 속성으로 pay_fee 및 class_type, times_a_week, class_term 담긴 pay_class 프로퍼티(추가)
-function setPaymentClassInfo(obj, key) {
-  const payClass = {}  
-  console.log('classkey :', key);
+function setPaymentClassInfo(obj, key) { //이거 원본 수정될수도있는건가
+  // console.log(obj, key)
+  const payClass = {
+    class_type : classDic[key].class_type,
+    times_a_week : classDic[key].times_a_week,
+    class_term : classDic[key].class_term,
+  } //여기서 classDic의 참조가 넘어와서 망가질가능성있음
+  obj.pay_class = payClass
+  // console.log(obj.pay_method);
+  // console.log(classDic[key][`${obj.pay_method}_price`]);
+  obj.pay_fee = classDic[key][`${obj.pay_method}_price`]
+  console.log(obj);
 }
 
 
