26fd93d6c765d6378470e1a4815c302465cebd61
uno1547 <63580165+uno1547@users.noreply.github.com>
2024-07-15 01:15:39 +0900

    pagination complete

--- js/pagination.js
+++ js/pagination.js
@@ -2,126 +2,85 @@
 구성요소
 
 현재 element개수
-보여줄 최대 element개수
-element로 인해 생기는 page개수
+page 당 보여줄 최대 element개수
+element로 인해 생기는 총 page개수
 현재 page number
 
-ex) elNum = 9, maxNum = 5
-    pageNum = maxElNum / elNum
 */
-const els = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-// const els = document.querySelectorAll('tr')
+// const els = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+const els = new Array(8).fill(0).map((val, idx) => idx + 1)
+console.log(els);
+
 class Pagination{
   constructor() {
-    // elNum, pageNum등을 계산후 초기화
-    this.elNum = els.length //9
-    this.maxElNum = 5 //5
+    // 현재 el개수, 페이지당최대el개수 >> pageNum 계산
+    this.elNum = els.length 
+    this.maxElNum = 5
     this.pageNum = (this.elNum % this.maxElNum == 0) ? Math.floor(this.elNum / this.maxElNum) : Math.floor(this.elNum / this.maxElNum) + 1
-    this.maxPageNum = 5
+    // this.maxPageNum = 5
 
     this.curPageNum = 1
   }
   initPaginationBar() {
     // 계산된 pageNum을 통해 indicator개수세팅
     const pageIndicatorDiv = document.querySelector("#pagination #page-indicator")
-    if(this.pageNum < this.maxPageNum) { // 계산된 page수 < 최대 page수(5)
-      for(let i = 0; i < this.pageNum; i++) {
-        pageIndicatorDiv.innerHTML += `<div class="page-btn">
-          <span class ="page-num">${i + 1}</span>
-        </div>
-        `
-      }
-    } else {
-      for(let i = 0; i < this.maxPageNum; i++) {
-        pageIndicatorDiv.innerHTML += `<div class="page-btn">
-          <span class ="page-num">${i + 1}</span>
-        </div>
-        `
-      }
+    for(let i = 0; i < this.pageNum; i++) {
+      pageIndicatorDiv.innerHTML += `<div class="page-btn">
+        <span class ="page-num">${i + 1}</span>
+      </div>
+      `
     }
-
+    // 숫자 직접클릭시 페이지 이동 가능하게 리스너 추가
     const pageBtnElements = pageIndicatorDiv.querySelectorAll(".page-btn")
     pageBtnElements.forEach((btnNode) => {
-      // console.log(btnNode);
       btnNode.addEventListener('click', () => {
         this.curPageNum = Number(btnNode.textContent)
-        console.log(`직접이동한 curPageNum ${this.curPageNum}`);
         this.styleCurpageBtn()
         this.showCurrentPageItems()
       })
     })
 
-
+    // 최초 current 페이지 스타일적용 및 해당 page item 표시
     this.styleCurpageBtn()
     this.showCurrentPageItems()
+
     const prevBtn = document.querySelector('#pagination button#prev')
     const nextBtn = document.querySelector('#pagination button#next')
-    // prevBtn.disabled = true
-    // console.log(this.paginationBarController);
     prevBtn.addEventListener('click', () => {
-      // this.curPageNum -= 1
-      // console.log(this.curPageNum);
-
-
       const changedCurPageNum = this.curPageNum - 1  
-      // console.log(changedCurPageNum)
       if(changedCurPageNum > 0) {
         this.curPageNum -= 1
         this.styleCurpageBtn()
         this.showCurrentPageItems()
       }
-      // console.log(this.curPageNum);
     })
     nextBtn.addEventListener('click', () => {
-      // this.curPageNum += 1
-      // console.log(this.curPageNum);
-
-
       const changedCurPageNum = this.curPageNum + 1  
-      // console.log(changedCurPageNum);
       if(changedCurPageNum <= this.pageNum) {
         this.curPageNum += 1
         this.styleCurpageBtn()
         this.showCurrentPageItems()
       }
-      // console.log(this.curPageNum);
     })
   }
-
-  
-  // paginationBarController() {
-  //   const changedCurPageNum = this.curPageNum - 1
-  //   if((changedCurPageNum > 0) && (changedCurPageNum < this.pageNum)) {
-  //     console.log();
-  //   }
-  //   console.log('click');
-  //   // this.styleCurpageBtn()
-  // }
-
+  // .current 에 스타일 추가
   styleCurpageBtn() {
     const pageIndicatorDiv = document.querySelector("#pagination #page-indicator")
     const prevCurBtnEl = pageIndicatorDiv.querySelector(".current")
     if(!prevCurBtnEl) {
       const firstPageBtn = document.querySelector(".page-btn:first-child")
-      // console.log(firstPageBtn);
       firstPageBtn.classList.add("current")
       return
     }
-    // 갱신된 curPageNum에 대해 표시 
+    // 갱신된 curPageNum에 대해 스타일  
     prevCurBtnEl.classList.remove("current")
     const btnEls = Array.from(pageIndicatorDiv.querySelectorAll("div.page-btn"))
     const curBtnEl = btnEls.filter((node) => node.textContent == this.curPageNum)[0]  
     curBtnEl.classList.add("current")
-    // console.log(btnEls);
-    // const curBtnEl = btnEls
-    // console.log(prevCurBtnEl);
-    // prevCurBtnEl.classList.remove("current")
-
     /*
     */
-    // btnEls.forEach((node) => console.log(node.textContent))
   }
-
+  // curPageNum에 대해 item추가
   showCurrentPageItems() {
     const startIdx = this.maxElNum * (this.curPageNum - 1)
     const endIdx = this.maxElNum * this.curPageNum
@@ -134,12 +93,11 @@ class Pagination{
       </tr>`
     })
 
-    const tableDiv = document.querySelector("div.inner table")
+    const tableDiv = document.querySelector("div.inner table.list-val")
     tableDiv.innerHTML = ""
     strOfItems.forEach((itemStr) => {
       tableDiv.innerHTML += itemStr
     })
-    // console.log(slicedItems);
   }
 }
 const paginationObj = new Pagination()
